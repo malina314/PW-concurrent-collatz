c81943f80af0343679d9ca200cbc430c97968ad9
Mateusz Malinowski <malina314@op.pl>
2022-02-09 16:23:12 +0100

    implements TeamNewThreads

--- teams.cpp
+++ teams.cpp
@@ -1,15 +1,44 @@
 #include <utility>
 #include <deque>
 #include <future>
+#include <vector>
 
 #include "teams.hpp"
 #include "contest.hpp"
 #include "collatz.hpp"
 
+void callCalcCollatz(const InfInt &n, uint64_t &res) {
+    res = calcCollatz(n);
+}
+
 ContestResult TeamNewThreads::runContestImpl(ContestInput const & contestInput)
 {
     ContestResult r;
-    //TODO
+    r.resize(contestInput.size());
+    uint64_t idx = 0;
+
+    if (this->getSharedResults()) {
+        //TODO
+    } else {
+        auto size = this->getSize();
+        std::vector<std::thread> threads(size);
+        size_t thread_idx = 0;
+        for (const InfInt &singleInput : contestInput) {
+            if (threads[thread_idx].joinable()) {
+                threads[thread_idx].join();
+            }
+            threads[thread_idx] = this->createThread(callCalcCollatz, singleInput, std::ref(r[idx]));
+            idx++;
+            thread_idx = (thread_idx + 1) % size;
+        }
+
+        for (auto &t : threads) {
+            if (t.joinable()) {
+                t.join();
+            }
+        }
+    }
+
     return r;
 }
 
