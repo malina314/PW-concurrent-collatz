218a64b944323aeb11101507efbfb3844a18071a
Mateusz Malinowski <malina314@op.pl>
2022-02-10 14:30:33 +0100

    implement blocking SharedResults

--- sharedresults.hpp
+++ sharedresults.hpp
@@ -1,9 +1,28 @@
 #ifndef SHAREDRESULTS_HPP
 #define SHAREDRESULTS_HPP
 
+#include "lib/infint/InfInt.h"
+
+#include <mutex>
+
 class SharedResults
 {
-//TODO
+    std::map<InfInt, uint64_t> results;
+    std::mutex m;
+
+public:
+    uint64_t getResult(const InfInt &n) {
+        std::lock_guard<std::mutex> lock(m);
+        auto it = results.find(n);
+
+        if (it == results.end()) {
+            uint64_t r = calcCollatz(n);
+            results[n] = r;
+            return r;
+        }
+
+        return it->second;
+    }
 };
 
 #endif
