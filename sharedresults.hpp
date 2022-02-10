1b6e3fb63df8e8835a7eebf5d09e904a3df3d9b6
Mateusz Malinowski <malina314@op.pl>
2022-02-10 14:59:42 +0100

    improve SharedResults

--- sharedresults.hpp
+++ sharedresults.hpp
@@ -7,12 +7,33 @@
 
 class SharedResults
 {
+    static constexpr int N = 100'000;
+
     std::map<InfInt, uint64_t> results;
-    std::mutex m;
+    std::mutex map_mutex, array_mutex;
+    uint64_t *small_results;
 
 public:
+    SharedResults() : results(), map_mutex(), array_mutex() {
+        small_results = new uint64_t[N]();
+    }
+
     uint64_t getResult(const InfInt &n) {
-        std::lock_guard<std::mutex> lock(m);
+        if (n < InfInt(N)) {
+            int small_n = n.toInt();
+
+            if (small_results[small_n] != 0) {
+                return small_results[small_n];
+            }
+
+            std::lock_guard<std::mutex> lock(array_mutex);
+
+            small_results[small_n] = calcCollatz(n);
+
+            return small_results[small_n];
+        }
+
+        std::lock_guard<std::mutex> lock(map_mutex);
         auto it = results.find(n);
 
         if (it == results.end()) {
@@ -22,6 +43,10 @@ public:
         }
 
         return it->second;
+    }
+
+    ~SharedResults() {
+        delete [] small_results;
     }
 };
 
